2692e8344c504e3106144e348754d8f517961094
mk-pmb <mk-pmb@users.noreply.github.com>
2016-08-30 19:43:29 +0200

    v0.1.1

--- README.md
+++ README.md
@@ -6,15 +6,100 @@ misc regexp utils.
 
 Usage
 -----
-see [doc/demo/usage.js](doc/demo/usage.js)
-:TODO:
+For examples see [test.js](test.js).
 
 
-```javascript
-var rxu = require('rxu');
-D.result  = rxu(null);
-D.expect('===',           null);
+### rxu(rgx, text)
+Try to match RegExp `rgx` on string `text`, with fancy extras.
+Returns the match if there was one, or `false` otherwise.
+The latter is chosen so you can safely access number properties
+of `rxu()`'s result.
+
+  * If `rgx` is an array, it's `rxu.join()`ed, see below.
+  * In case there is a match,
+    * … it's extended with `rxu.contextSlots(match, text)`.
+    * … it's cached, see below.
+
+
+### rxu(slot)
+Get property `slot` (string or number) from the cached match object.
+If the slot contains a function, call it and return its result.
+
+
+### rxu(null)
+Clear `rxu()`'s cache.
+Do this if you matched sensitive information that you need to hide from
+other modules that run in the same VM.
+You probably can't if they're evil, but you might avoid accidential disclosure.
+This does not affect any other caches that your VM might have created,
+maybe by some aspect of its RegExp implementation,
+like [`RegExp.input` in Firefox][mdn-regexp-input].
+
+
+### rxu.quotemeta(text)
+Return a version of `text` that has a backslash added in front of each
+character that looks like it could has a special meaning in RegExp syntax,
+and whitespace characters. The exact list can be found in `rxu.rgxMagicChars`.
+Don't change it, or you'll break other modules that run in the same VM.
+
+
+### rxu.replacer(rWhat, rWith)
+```js
+  return function (text) { return String(text).replace(rWhat, rWith); };
 ```
+Construct handy iterators.
+
+
+### rxu.ifMatch(text, rgx, thenFunc, elseFunc)
+```js
+  return (match ? (thenFunc ? thenFunc(match, text) : match)
+                : (elseFunc ? elseFunc(text)        : false));
+```
+
+
+### rxu.body(rx), rxu.flags(rx)
+Get the body part or flags from a regexp. Uses `.source` and `.flags`
+where available, with fallback to string parsing.
+
+
+### rxu.join(rxs, flags)
+Given an array `rxs` of RegExp bodies given as strings
+(and/or as RegExps, which will be converted to strings using `rxu.body`),
+make a new RegExp from the concatenated body parts and (if given) `flags`.
+
+
+### rxu.end(m), rxu.before(m), rxu.after(m)
+Assuming that `m` is a result object from a successful RegExp match,
+determine…
+  * `end`:   the position of the first character after the match.
+  * `before`: the text before the match.
+  * `after`:  the text after the match.
+
+
+### rxu.contextSlots(m)
+Assuming that `m` is a result object from a successful RegExp match,
+sets some handy additional slots on `m`:
+
+  * `m['@']`: (int) Position of the match. Alias for `m.index`.
+  * `m.end`: (int) Position of the first character after the match.
+  * `m['*']`: (func) Returns `m.input`.
+  * `m['<']`, `m.before`: (func) Returns the text before the match.
+  * `m['>']`, `m.after`: (func) Returns the text after the match.
+
+
+### rxu.args2match(arguments)
+When you call `.replace()` on some string with the 2nd argument being your
+replacer function, that function won't receive a proper match result object,
+just unnamed arguments. This function converts that `arguments` Array-like
+object to a proper match result object, so you can use it with other
+`rxu` functions.
+
+
+
+
+
+  [mdn-regexp-input]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/input
+
 
 
 License
--- rxu.js
+++ rxu.js
@@ -2,13 +2,188 @@
 /* -*- tab-width: 2 -*- */
 'use strict';
 
-var EX = module.exports;
+//##### BEGIN short helper funcs #####//
+function identity(x) { return x; }
+function isStr(x) { return ((typeof x) === 'string'); }
+function isNum(x) { return ((typeof x) === 'number'); }
+function isRgx(x) { return (x instanceof RegExp); }
+function isArr(x) { return (x instanceof Array); }
+function isFunc(x) { return (x instanceof Function); }
+//##### ENDOF short helper funcs #####//
 
-EX.id = function identity(x) { return x; };
 
-EX.ifMatch = function rxu_ifMatch(text, rgx, thenFunc, elseFunc) {
+//##### BEGIN official API #####//
+
+function rxu(rgx, text) {
+  var match;
+  if (arguments.length === 1) {
+    switch (rgx && typeof rgx) {
+    case '':
+    case 'string':
+    case 'number':
+      rgx = rxu.cache[rgx];
+      if (isFunc(rgx)) { rgx = rgx(); }
+      return rgx;
+    case null:
+      rxu.cache = false;
+      return;
+    case 'object':    // probably RegExp or Array of RegExp parts
+      break;
+    default:
+      throw new Error('Unsupported slotname rxu("' + String(rgx) + '")');
+    }
+  }
+  if (isArr(rgx)) { rgx = rxu.join(rgx); }
+  match = String(text).match(rgx);
+  if (!match) { return false; }
+  rxu.contextSlots(match, text);
+  rxu.cache = match;
+  return match;
+}
+
+rxu.quotemeta = function (text) {
+  return String(text).replace(rxu.rgxMagicChars, '\\$1');
+};
+rxu.rgxMagicChars = /([\!\#\$\(\)\*\+\-\.\/\?\[\\\]\^\{\|\}\s\n])/g;
+
+rxu.replacer = function (rWhat, rWith) {
+  return function (text) { return String(text).replace(rWhat, rWith); };
+};
+
+rxu.ifMatch = function rxu_ifMatch(text, rgx, thenFunc, elseFunc) {
   var match = rgx.exec(text);
   rxu_ifMatch.m = (match || false);
   return (match ? (thenFunc ? thenFunc(match, text) : match)
-                : (elseFunc ? elseFunc(text) : false));
+                : (elseFunc ? elseFunc(text)        : false));
 };
+
+rxu.body = function rxu_body(rx) {
+  if (!rx) { return ''; }
+  rx = rxu.body.raw(rx);
+  if (rx === '(?:)') { return ''; }
+  rx = rx.replace(/\n|\r/g, rxu.jsonquot);    // upgrade ES3+4 to ES5
+  return rx;
+};
+
+rxu.flags = function rxu_flags(rx) {
+  if (!isRgx(rx)) { return ''; }
+  if (isStr(rx.flags)) { return rx.flags; }
+  rx = String(rx);
+  return rx.substr(rx.lastIndexOf('/') + 1, rx.length);
+};
+
+rxu.join = function rxu_join(rxs, flags) {
+  if (isStr(rxs)) { return rxs; }
+  if (isRgx(rxs)) { return rxu.body(rxs); }
+  if (isArr(rxs)) { return new RegExp(rxs.map(rxu_join).join(''), flags); }
+  throw new Error('Expected array of {RegExp|string} as arg 1');
+};
+
+rxu.end = function (m) {
+  if (!m) { rxu.args2match.hint(null, m); }
+  if (!isStr(m[0])) { rxu.args2match.hint(0, m); }
+  if (!isNum(m.index)) { rxu.args2match.hint('index', m); }
+  return (m.index + m[0].length);
+};
+
+rxu.before = function (m) {
+  if (!m) { rxu.args2match.hint(null, m); }
+  if (!isStr(m.input)) { rxu.args2match.hint('input', m); }
+  if (!isNum(m.index)) { rxu.args2match.hint('index', m); }
+  return m.input.slice(0, m.index);
+};
+
+rxu.after = function (m) {
+  if (!m) { rxu.args2match.hint(null, m); }
+  if (!isStr(m.input)) { rxu.args2match.hint('input', m); }
+  if (!isStr(m[0])) { rxu.args2match.hint(0, m); }
+  if (!isNum(m.index)) { rxu.args2match.hint('index', m); }
+  return m.input.slice(m.index + m[0].length, m.input.length);
+};
+
+rxu.contextSlots = function (m) {
+  m['@'] = m.index;
+  m.end = m.index + m[0].length;
+  m['*'] = function () { return m.input; };
+  m['<'] = m.before = rxu.before.bind(null, m);
+  m['>'] = m.after = rxu.after.bind(null, m);
+  return m;
+};
+
+rxu.args2match = function (args) {
+  if (!rxu.args2match.maybe(args)) {
+    throw new Error("That doesn't look like replacer arguments: " +
+      String(args) + (args instanceof Object ? JSON.stringify(args) : ''));
+  }
+  args = Array.prototype.slice.call(args);
+  args.input = args.pop();
+  args.index = args.pop();
+  return args;
+};
+
+rxu.args2match.maybe = function (obj) {
+  return Boolean(obj
+    && isNum(obj.length)
+    && isNum(obj[obj.length - 2])
+    && isStr(obj[obj.length - 1])
+    && isStr(obj[0]));
+};
+
+
+
+//##### ENDOF official API #####//
+
+
+
+
+
+
+//##### BEGIN internal utility funcs #####//
+
+
+rxu.jsonquot = function (x) {
+  x = JSON.stringify(String(x));
+  return x.substr(1, x.length - 2);
+};
+
+rxu.body.raw = function (rx, body) {
+  if (!isRgx(rx)) { return String(rx); }
+  body = rx.source;
+  if (isStr(body)) { return body; }
+  return String(rx).replace(/^\//, '').replace(/\/[a-z]*$/, '');
+};
+
+rxu.args2match.hint = function (prop, obj) {
+  var value = (prop ? obj[prop] : obj);
+  prop = (prop ? ('Property ' + (isNum(prop) ? '[' + prop + ']' : '.' + prop
+                  ) + ' missing or of wrong type, value: ')
+               : 'Expected a match result object but got ');
+  prop += (isStr(value) ? JSON.stringify(value) : String(value));
+  if (rxu.args2match.maybe(obj)) {
+    prop += '; Is that really a regexp match object?' +
+      ' It looks like it might be replacer function arguments.';
+  }
+  throw new Error(prop);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//##### ENDOF internal utility funcs #####//
+
+module.exports = rxu;
