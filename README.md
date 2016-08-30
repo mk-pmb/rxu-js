93ed712c04542698d1b45f2f98634ff857bf0465
mk-pmb <mk-pmb@users.noreply.github.com>
2016-08-30 14:10:41 +0200

    ifMatch

---
+++ README.md
@@ -0,0 +1,22 @@
+﻿
+rxu
+===
+misc regexp utils.
+
+
+Usage
+-----
+see [doc/demo/usage.js](doc/demo/usage.js)
+:TODO:
+
+
+```javascript
+var rxu = require('rxu');
+D.result  = rxu(null);
+D.expect('===',           null);
+```
+
+
+License
+-------
+ISC
---
+++ rxu.js
@@ -0,0 +1,14 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var EX = module.exports;
+
+EX.id = function identity(x) { return x; };
+
+EX.ifMatch = function rxu_ifMatch(text, rgx, thenFunc, elseFunc) {
+  var match = rgx.exec(text);
+  rxu_ifMatch.m = (match || false);
+  return (match ? (thenFunc ? thenFunc(match, text) : match)
+                : (elseFunc ? elseFunc(text) : false));
+};
